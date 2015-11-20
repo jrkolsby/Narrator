77329563e91322225b3d200285b881547d485e32
James Kolsby <jrkolsby@mac.com>
2015-11-20 16:52:55 -0500

    Built UI

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-
-
 window.onload = function() {
+	if (!('webkitSpeechRecognition' in window)) {
+		upgrade();
+	} else {
+		var recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
+		recognition.interimResults = true;
 
+		recognition.onstart = function() {
+			console.log("hi");
+		}
+		recognition.onresult = function(event) {
+			console.log(event);
+		}
+		recognition.onerror = function(event) {}
+		recognition.onend = function() {}
+	}
 }
