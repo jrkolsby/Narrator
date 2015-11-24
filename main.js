305f0547dc0824a2f97cc9dd06f795e754b5c2b3
James Kolsby <jrkolsby@mac.com>
2015-11-23 20:29:59 -0500

    Added handling for interim results

--- main.js
+++ main.js
@@ -1,62 +1,108 @@
 var Transcript = function(text, voice) {
+
 	var script = {
-		read: "",
-		certain: "",
-		uncertain: ""
+		read: "", // Words spoken by TalkInterface
+		count: "", // Aggregates words added to unread until said in groups of >=3
+		unread: "", // Words not yet spoken by TalkInterface
+		certain: "", // Confident words in the current phrase
+		uncertain: "" // Unconfident words in the current phrase
 	}
 
 	var updateTextWithScript = function() {
-		text.updateText(script.read,
-						script.certain,
-						script.uncertain);
+		text.setRead(script.read);
+		text.setUnread(script.unread);
+		text.setUncertain(script.uncertain);
+
+		console.log(script);
 	}
 
-	var addToRead = function(t) {
-		script.read += t;
-		script.certain = script.certain.substring(t.length,
-												  script.certain.length);
-		updateTextWithScript();		
+	var compareStrings = function(a, b) {
+
+		// From: http://stackoverflow.com/questions/8024102/javascript-compare-strings-and-get-end-difference
+
+		var firstOccurance = b.indexOf(a);
+		var newString = "";
+
+		if (firstOccurance >= 0) {
+
+			var aLength = a.length;
+
+			if (firstOccurance == 0) { newString = b.substring(aLength) }
+			else { newString = b.substring(0, firstOccurance) +
+							   b.substring(firstOccurance + aLength) }
+		} else { newString = "" }
+
+		return newString;
 	}
 
-	this.addToCertain = function(t) {
-		script.certain += t
-		updateTextWithScript();
+	var wordCount = function(s) {
+		return s.replace(/\s+$/, '').split(/\s+/).length-1;
+	}
 
-		if (t.replace(" ", "").length > 0) {
-			voice.say(t, function(e) { addToRead(e.utterance.text) });
+	var GROUPING_MINIMUM = 5;
+
+	this.handleResult = function(event) {
+
+		var i = event.resultIndex,
+			resultLength = event.results.length-i,
+			isFinal = event.results[i].isFinal,
+			certain,
+			uncertain;
+
+		if (isFinal) {
+			certain = event.results[i][0].transcript;
+			uncertain = "";
+		} else if (resultLength > 1) {
+			certain = event.results[i][0].transcript;
+			uncertain = event.results[i+1][0].transcript;
+		} else {
+			certain = "";
+			uncertain = event.results[i][0].transcript;
 		}
-	}
 
-	this.setCertain = function(t) {
-		script.certain += t;
+		script.uncertain = uncertain;
 		updateTextWithScript();
-	}
 
-	this.setUncertain = function(t) {
-		script.uncertain = t
-		updateTextWithScript();
+		if (certain !== "") {
+			var certainAddition = compareStrings(script.certain, certain);
+
+			script.certain += certainAddition;
+			script.unread += certainAddition;
+			script.count += certainAddition;
+
+			updateTextWithScript();
+
+			if (wordCount(script.count) >= GROUPING_MINIMUM || isFinal) {
+				voice.say(script.count, function(e) {
+					var spoken = e.utterance.text;
+					var newUnread = compareStrings(spoken, script.unread);
+
+					script.read += spoken;
+					script.unread = newUnread;
+
+					updateTextWithScript();
+				});
+				script.count = "";
+			}
+
+			if (isFinal) { script.certain = "" }
+		}
 	}
 }
 
 var TextInterface = function() {
 	var element = {
-		read: document.getElementById("spoken"),
+		read: document.getElementById("read"),
 		alert: document.getElementById("alert"),
-		certain: document.getElementById("certain"),
+		unread: document.getElementById("unread"),
 		uncertain: document.getElementById("uncertain")
 	}
 
-	var setCertain = function(t) { element.certain.innerHTML = t }
-	var setUncertain = function(t) { element.uncertain.innerHTML = t }
-	var setRead = function(t) { element.read.innerHTML = t }
+	this.setUncertain = function(t) { element.uncertain.innerHTML = t }
+	this.setUnread = function(t) { element.unread.innerHTML = t }
+	this.setRead = function(t) { element.read.innerHTML = t }
 
 	this.setAlert = function(t) { element.alert.innerHTML = t }
-
-	this.updateText = function(read, certain, uncertain) {
-		setRead(read);
-		setCertain(certain);
-		setUncertain(uncertain);
-	}
 
 	this.UPGRADE_ALERT = "Requires Google Chrome version 24 or above";
 	this.ERROR_ALERT = "Speech recognition error: ";
@@ -64,7 +110,6 @@ var TextInterface = function() {
 
 var TalkInterface = function() {
 
-	var script = "";
 	var voices = window.speechSynthesis.getVoices();
 
 	var VOICE_RATE = 1,
@@ -73,13 +118,13 @@ var TalkInterface = function() {
 		URI = 'native';
 
 	this.say = function(u, ended) {
+
 		var msg = new SpeechSynthesisUtterance();
 
 		msg.text = u;
 		msg.pitch = VOICE_PITCH;
 		msg.rate = VOICE_RATE;
 		msg.voiceURI = URI;
-
 		msg.lang = LANG;
 
 		msg.onend = function(e) { ended(e) }
@@ -87,12 +132,12 @@ var TalkInterface = function() {
 		window.speechSynthesis.speak(msg);
 	}
 }
+
 window.onload = function() {
 
-	var text = new TextInterface();
-	var voice = new TalkInterface();
-
-	var script = new Transcript(text, voice);
+	var text = new TextInterface(),
+		voice = new TalkInterface(),
+		script = new Transcript(text, voice);
 
 	if (!('webkitSpeechRecognition' in window)) { text.setAlert(text.UPGRADE_ALERT); }
 	else {
@@ -104,28 +149,17 @@ window.onload = function() {
 		recognition.onstart = function() {}
 
 		recognition.onend = function() {
-			console.log("end");
+			console.log("restart");
 			recognition.start();
-			script.addToCertain(" ");
 		}
 
 		recognition.onresult = function(event) {
-			console.log("result");
-			var uncertainAggregate = "";
-			for (var i = event.resultIndex; i < event.results.length; ++i) {
-				var addition = event.results[i][0].transcript;
-				if (event.results[i].isFinal) { script.addToCertain(addition) }
-				else { uncertainAggregate += addition }
-			}
-			script.setUncertain(uncertainAggregate);
+			script.handleResult(event);
 		}
 
 		recognition.onerror = function(event) {
 			text.setAlert(text.ERROR_ALERT + event.error);
 		}
-
-		recognition.onspeechstart = function() {}
-		recognition.onspeechend = function() {}
 
 		recognition.start();
 	}
